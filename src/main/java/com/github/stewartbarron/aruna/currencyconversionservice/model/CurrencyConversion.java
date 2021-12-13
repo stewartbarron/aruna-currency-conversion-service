aa4980d858d301dc0aa3f951405d92c6e518a31e
Stewart Barron <stewartbarron@Stewarts-iMac.local>
2021-12-13 20:45:08 +0000

    updated to call exchange service

--- src/main/java/com/github/stewartbarron/aruna/currencyconversionservice/model/CurrencyConversion.java
+++ src/main/java/com/github/stewartbarron/aruna/currencyconversionservice/model/CurrencyConversion.java
@@ -1,2 +1,20 @@
-package com.github.stewartbarron.aruna.currencyconversionservice.model;public class CurrencyConversion {
+package com.github.stewartbarron.aruna.currencyconversionservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyConversion {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private BigDecimal quantity;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
 }
